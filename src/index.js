8747d6972802472a906abcb4237430d2a523c19f
saadaouad <aouadsaad05@gmail.com>
2018-05-27 15:18:29 +0000

    Change structure app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MainComponent from './main-component/index.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MainComponent />, document.getElementById('root'));
 registerServiceWorker();
--- src/main-component/index.js
+++ src/main-component/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
-class App extends Component {
+class MainComponent extends Component {
   render() {
     return (
       <div className="App">
@@ -10,12 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React and D3</h1>
         </header>
-        {/* <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
       </div>
     );
   }
 }
 
-export default App;
+export default MainComponent;
