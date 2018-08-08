a480b962ca633e108691ca66d7aa0ec3f931364b
saadaouad <aouadsaad05@gmail.com>
2018-08-08 02:15:58 +0100

    Add specific asserts output for main component

--- src/MainComponent/__tests__/MainComponent.spec.js
+++ src/MainComponent/__tests__/MainComponent.spec.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import MainComponent from '../index.js';
+import logo from '../logo.svg';
+import LineChartComponent from '../../LineChartComponent/index.js';
+import BarChartComponent from '../../BarChartComponent/index.js';
 
 it('renders without crashing', () => {
-  shallow(<MainComponent />);
+  const wrapper = shallow(<MainComponent />);
+  const officialLogo = <img src={logo} className="App-logo" alt="logo" />
+  const welcome = <h1 className="App-title">Welcome to React and D3</h1>;
+  const lineChart = <LineChartComponent />;
+  const barChart = <BarChartComponent />;
+  expect(wrapper.contains(officialLogo)).toEqual(true);
+  expect(wrapper.contains(welcome)).toEqual(true);
+  expect(wrapper.contains(lineChart)).toEqual(true);
+  expect(wrapper.contains(barChart)).toEqual(true);
 });
